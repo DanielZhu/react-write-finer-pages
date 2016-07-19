d68988cf6c9ad2b0bdeaa423e849c6f5e8111c0e
DanielZhu <enterzhu@gmail.com>
2016-07-19 13:45:53 +0800

    Add ins

--- entry.js
+++ entry.js
@@ -2,21 +2,26 @@ import React from 'react';
 import {render} from 'react-dom';
 import {Router, Route, Redirect, IndexRoute, Link, hashHistory} from 'react-router'
 
-// Customized Components
+// Common Components
+import Spin from './src/components/Spin/Spin'
+import NotFound from './src/components/NotFound/NotFound'
+import App from './src/components/App/App'
+
+// Trello Components
 import TrelloAppSkeleton from './src/trello/components/App/App'
 import TrelloCol from './src/trello/components/Col/Col'
 import TrelloBoardBriefBox from './src/trello/components/BoardBriefBox/BoardBriefBox'
 
-import Spin from './src/components/Spin/Spin'
-import NotFound from './src/components/NotFound/NotFound'
-import App from './src/components/App/App'
-// import InstagramApp from './src/instagram/index'
+// Instagram Components
+import InstagramAppSkeleton from './src/instagram/components/App/App'
+// import InstagramStream from './src/instagram/components/Stream/Stream'
 
 // redirectApp() {
-  setTimeout(() => {
-    window.location.href = '#/trello';
-  }, 1500);
+  // setTimeout(() => {
+  //   window.location.href = '#/trello';
+  // }, 1500);
 // }
+
 render((
   <Router history={hashHistory}>
     <Route path='/' component={App}>
@@ -24,6 +29,8 @@ render((
       <Route path='trello' component={TrelloAppSkeleton} >
         <IndexRoute component={TrelloBoardBriefBox} />
         <Route path='b/:shortlink/:name' component={TrelloCol} />
+      </Route>
+      <Route path='instagram' component={InstagramAppSkeleton} >
       </Route>
       <Route path='*' component={NotFound} />
     </Route>
